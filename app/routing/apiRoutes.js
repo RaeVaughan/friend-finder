4ea9189d0c44d90c6a0f184c9eafc05e6e11a860
RaeVaughan <rachelev.tx@gmail.com>
2017-07-16 12:25:09 -0500

    fixed posting functionality; started work on friend score comparison

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,7 +11,33 @@ module.exports = function(app) {
 
 	//POST requests to submit data to the server
 	app.post("/api/friends", function(req, res) {
+		console.log("req.body ", req.body);
+		console.log("friendData ", friendData);
+		//loop through scores, parsing into integers
+		//save req.body.scores to new scores array of integers
+		var scores = req.body.scores;
+		for(i = 0; i < scores.length; i++) {
+			scores[i] = parseInt(scores[i]);
+		}
+		console.log("scores: ", scores);
+		var minDiff = 0;
+
+		//getting differences
+		for(i = 0; i < friendData.length; i++) {
+
+		}
+
+		// if (friendData.includes(req.body)) {
+		// 	console.log("match");
+		// }
+
 		friendData.push(req.body);
-	})
+		//send back friend match to go in the modal
+
+
+		res.json(friendData);
+	});
+
+
 }
 
